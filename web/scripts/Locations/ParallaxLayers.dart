fb7712e6a7534ce1aa2a53d8c5f5e493ab611640
jadedResearcher <gtg287y@gmail.com>
2018-08-19 15:43:32 -0400

    looping should work

--- web/scripts/Locations/ParallaxLayers.dart
+++ web/scripts/Locations/ParallaxLayers.dart
@@ -14,12 +14,16 @@ class ParallaxLayer {
     bool removeMePlease = false;
 
     ParallaxLayer(String this.imageLocation, PhysicalLocation this.parent, int this.speed) {
-        image = new ImageElement(src: imageLocation);
-        image.style.zIndex = "$speed"; //auto sorts things by speed
-        image.style.left = "0px";
-        image.classes.add("parallaxLayer");
-        parent.container.append(image);
+        init();
         animate();
+    }
+
+    void init() {
+      image = new ImageElement(src: imageLocation);
+      image.style.zIndex = "$speed"; //auto sorts things by speed
+      image.style.left = "0px";
+      image.classes.add("parallaxLayer");
+      parent.container.append(image);
     }
 
     Future<Null> animate() async{
@@ -48,19 +52,46 @@ class ParallaxLayer {
 //handles auto scrolling through css
 //only half is displayed at once
 class ParallaxLayerLooping extends ParallaxLayer{
-  ParallaxLayerLooping(String imageLocation, PhysicalLocation parent, int speed) : super(imageLocation, parent, speed);
+    ImageElement image2;
+
+    ParallaxLayerLooping(String imageLocation, PhysicalLocation parent, int speed) : super(imageLocation, parent, speed);
+
+    Future<Null> animate() async{
+        if(removeMePlease) return;
+        move();
+        await window.animationFrame;
+        new Timer(new Duration(milliseconds: frameRate), () => animate());
+    }
+
+    void init() {
+        super.init();
+        image2 = new ImageElement(src: imageLocation);
+        image2.style.zIndex = "$speed"; //auto sorts things by speed
+        image2.style.left = "800px"; //at the right side
+        image2.classes.add("parallaxLayer");
+        parent.container.append(image2);
+    }
 
     @override
     void move() {
         int x = int.parse(image.style.left.replaceAll("px", ""));
         x = x - speed;
         //if i am less than -width/2, go back to start
-        double max = -1* parent.width;
+        int max = -1* parent.width;
         if(x<max) {
             print("resetting");
             x = 0;
         }
-        image.style.left = "${x}px";
+
+        int x2 = int.parse(image2.style.left.replaceAll("px", ""));
+        x2 = x2 - speed;
+        //if i am less than -width/2, go back to start
+        int max2 = -1* parent.width;
+        if(x2<max2) {
+            print("resetting");
+            x2 = 0;
+        }
+        image2.style.left = "${x2}px";
     }
 }
 
