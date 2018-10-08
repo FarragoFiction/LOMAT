777eecd692fd2d4c261da34d03cca900f625a89f
jadedResearcher <gtg287y@gmail.com>
2018-10-07 20:22:37 -0400

    time to wire up hunting and travel buttons

--- web/main.dart
+++ web/main.dart
@@ -7,5 +7,5 @@ Town town;
 DivElement div = querySelector('#output');
 void main() {
   String flavorTown = "You arrive in beautiful CITY2, the jewel of LOMAT. <br><Br>Or at least that's what you'd think if it were in its finished state. Sadly, it appears to have been shittly drawn by a WASTE or something, and everything in it is in test mode. <br><Br>Oh well, beats looking at a blank white screen, you suppose.";
-  town = new Town("city2",flavorTown,div, <LOMATNPC>[LOMATNPC.generateRandomNPC()]);
+  town = new Town("city2",flavorTown,div, <LOMATNPC>[LOMATNPC.generateRandomNPC(),LOMATNPC.generateRandomNPC(),LOMATNPC.generateRandomNPC()]);
 }
--- web/scripts/NPCs/LOMATNPC.dart
+++ web/scripts/NPCs/LOMATNPC.dart
@@ -43,6 +43,20 @@ class LOMATNPC {
         }
     }
 
+    static String seagullQuirk(String text) {
+        Random rand = new Random();
+        rand.nextInt();
+        text = text.toUpperCase();
+
+        if(rand.nextDouble() > 0.6) {
+            text = "SQWAWK!!!!! $text";
+        }else if(rand.nextBool()) {
+            text = "$text SQWAWK!!!!!";
+        }
+        return text;
+
+    }
+
     //TODO not gonna use this for the real game or anything, but good for testing
     static LOMATNPC generateRandomNPC() {
         Random rand = new Random();
@@ -53,18 +67,22 @@ class LOMATNPC {
         LOMATNPC testNPC = new LOMATNPC("images/Seagulls/happy.gif","images/Seagulls/middle.gif","images/Seagulls/trepidation.gif", level);
 
         List<String> emotions = <String>[TalkyItem.HAPPY, TalkyItem.NEUTRAL, TalkyItem.SAD];
-        List<String> gameQuips = <String>["This game is going to be based on the retro game 'Oregeon Trail'.","This game will involve ferrying the 'souls of the dead' to their final resting place.","This game is the Land of Mists and Trails."];
+        List<String> gameQuips = <String>["This game is going to be based on the retro game 'Oregon Trail'.","This game will involve ferrying the 'souls of the dead' to their final resting place.","This game is the Land of Mists and Trails."];
         List<String> townQuips = <String>["This town is a default test town scribbled by JR.","This town is snowy, and has some huge trees in it.","This town will probably be redrawn by an actual artist at some point.","Everyone in this town can't figure out how to leave it."];
         List<String> meQuips = <String>["You are the Guide of Void.","You are the prophesied Hero who will guide the lost souls of this land to their final resting place.","Your horns are very nice."];
+        List<String> youQuips = <String>["I'm not DRESSED like a ghost, I AM a ghost.", "Behold my robes y/n?", "Because I AM a ghost!"];
 
 
-        TalkyResponse tr = new TalkyResponse(testNPC,new List<TalkyItem>(),rand.pickFrom(gameQuips), rand.pickFrom(emotions),null);
+        TalkyResponse tr = new TalkyResponse(testNPC,new List<TalkyItem>(),seagullQuirk(rand.pickFrom(gameQuips)), rand.pickFrom(emotions),null);
         TalkyQuestion question1 = new TalkyQuestion("What can you tell me about this game?",tr,level);
 
-        TalkyResponse tr2 = new TalkyResponse(testNPC,new List<TalkyItem>(),rand.pickFrom(townQuips), rand.pickFrom(emotions),null);
+        TalkyResponse tr2 = new TalkyResponse(testNPC,new List<TalkyItem>(),seagullQuirk(rand.pickFrom(townQuips)), rand.pickFrom(emotions),null);
         TalkyQuestion question2 = new TalkyQuestion("What can you tell me about this town?",tr2,level);
 
-        TalkyResponse tr3 = new TalkyResponse(testNPC,new List<TalkyItem>(),rand.pickFrom(meQuips), rand.pickFrom(emotions),null);
+        TalkyResponse trghost = new TalkyResponse(testNPC,new List<TalkyItem>(),seagullQuirk(rand.pickFrom(youQuips)), rand.pickFrom(emotions),null);
+        TalkyQuestion questionghost = new TalkyQuestion("Why are you dressed like a ghost?",trghost,level);
+
+        TalkyResponse tr3 = new TalkyResponse(testNPC,new List<TalkyItem>(),seagullQuirk(rand.pickFrom(meQuips)), rand.pickFrom(emotions),null);
         TalkyQuestion question3 = new TalkyQuestion("What can you tell me about me?",tr3,level);
 
         return testNPC;
