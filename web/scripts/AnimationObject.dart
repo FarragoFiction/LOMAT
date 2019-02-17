15c2c4b9c0264d8c237a841f49e99c046d99f236
jadedResearcher <gtg287y@gmail.com>
2019-02-17 18:34:40 -0500

    it works? needs optimization tho

--- web/scripts/AnimationObject.dart
+++ web/scripts/AnimationObject.dart
@@ -2,30 +2,37 @@ import 'dart:async';
 import 'dart:html';
 
 import 'package:CommonLib/Colours.dart';
+import 'package:CommonLib/Random.dart';
 import 'package:LoaderLib/Loader.dart';
+import 'package:RenderingLib/RendereringLib.dart';
 
 class AnimationObject {
     int numberFrames;
     int frameRateInMS;
     int width;
     int height;
-    List<AnimationLayer> layers;
+    List<AnimationLayer> layers = new List<AnimationLayer>();
     CanvasElement _canvasElement;
+    AnimationObject(int this.numberFrames, int this.frameRateInMS, int this.width, int this.height);
 
     //TODO write a test page for this
     CanvasElement get element {
         if(_canvasElement == null) {
             _canvasElement = new CanvasElement(width: width, height: height);
         }
+        renderLoop();
         return _canvasElement;
     }
 
     Future<Null> renderLoop() async {
         //TODO render each of your layers in order, await them so it doesn't render out oforder
         await window.animationFrame;
-        layers.forEach((AnimationLayer layer) {
-            layer.render(_canvasElement);
+        _canvasElement.context2D.clearRect(0,0, _canvasElement.width, _canvasElement.height);
+        layers.forEach((AnimationLayer layer) async {
+            await layer.render(_canvasElement);
         });
+        new Timer(new Duration(milliseconds: frameRateInMS), () => renderLoop());
+
     }
 
 }
@@ -33,27 +40,34 @@ class AnimationObject {
 class AnimationLayer {
     List<String> frameLocations;
     //lazy load each frame as you need it, only add to elements when its there, same order as locations
-    List<ImageElement> elements;
+    List<ImageElement> elements = new List<ImageElement>();
     Palette palette;
+    Palette paletteSource;
+
     int animationFrameIndex = 0;
 
-    AnimationLayer(List<String> this.frameLocations, Palette this.palette);
+    AnimationLayer(List<String> this.frameLocations, Palette this.paletteSource, Palette this.palette);
 
     void incrementIndex() {
         animationFrameIndex ++;
-        if(animationFrameIndex > frameLocations.length) {
+        if(animationFrameIndex > frameLocations.length-1) {
             animationFrameIndex = 0;
         }
     }
 
     Future<Null> render(CanvasElement canvas) async {
         ImageElement img;
-        if(animationFrameIndex > elements.length) {
+        if(animationFrameIndex > elements.length-1) {
             img = await Loader.getResource(frameLocations[animationFrameIndex]);
+            elements.add(img);
+        }else {
+            img = elements[animationFrameIndex];
         }
-        canvas.context2D.clearRect(0,0, canvas.width, canvas.height);
         canvas.context2D.drawImage(img,0,0);
-        //TODO handle swap colors
+        //CanvasElement canvas, Palette source, Palette replacement
+        if(palette != null) {
+            Renderer.swapPalette(canvas, paletteSource, palette);
+        }
         incrementIndex();
     }
 }
@@ -65,11 +79,34 @@ class GullAnimation  extends AnimationObject{
     static String  baseLocation = "images/Seagulls/";
     static String baseLocationBody = "${baseLocation}BodyFrames/";
     static String baseLocationHat = "${baseLocation}HatFrames/";
+
+    static  Palette get randomPalette {
+        Random rand = new Random();
+        return rand.pickFrom(birdColors);
+    }
+
+
+    static Palette redBird = new Palette()..add("edge",new Colour.fromStyleString("#430404"))..add("sheet",new Colour.fromStyleString("#e4bcc9"));
+    static Palette blueBird = new Palette()..add("edge",new Colour.fromStyleString("#002b48"))..add("sheet",new Colour.fromStyleString("#7579ff"));
+    static Palette yellowBird = new Palette()..add("edge",new Colour.fromStyleString("#3d300b"))..add("sheet",new Colour.fromStyleString("#ffe53d"));
+    static Palette greenBird = new Palette()..add("edge",new Colour.fromStyleString("#161c01"))..add("sheet",new Colour.fromStyleString("#a7ca37"));
+    static Palette purpleBird = new Palette()..add("edge",new Colour.fromStyleString("#0e021c"))..add("sheet",new Colour.fromStyleString("#9e65e3"));
+    static Palette jadeBird = new Palette()..add("edge",new Colour.fromStyleString("#021a0e"))..add("sheet",new Colour.fromStyleString("#56cf93"));
+    static Palette fuchsiaBird = new Palette()..add("edge",new Colour.fromStyleString("#260213"))..add("sheet",new Colour.fromStyleString("#dd93b6"));
+    static Palette bronzeBird = new Palette()..add("edge",new Colour.fromStyleString("#241300"))..add("sheet",new Colour.fromStyleString("#c2731f"));
+    static Palette whiteBird = new Palette()..add("edge",new Colour.fromStyleString("#002b48"))..add("sheet",new Colour.fromStyleString("#f5ffff"));
+
+    static List<Palette> birdColors = <Palette>[redBird,blueBird,yellowBird,greenBird,purpleBird,jadeBird,fuchsiaBird,bronzeBird,whiteBird];
     String hatBase;
     //TODO also has a palette for body
+    //f5ffff
+    //002b48  edge
+    Palette paletteSource = new Palette()..add("edge",new Colour.fromStyleString("#002b48"))..add("sheet",new Colour.fromStyleString("#f5ffff"));
+    Palette palette;
 
-    GullAnimation(String this.hatBase) {
-
+    GullAnimation(String this.hatBase):super(17, 20, 254, 288) {
+        layers.add(bodyLayer());
+        layers.add(hatLayer());
     }
 
     AnimationLayer bodyLayer() {
@@ -77,11 +114,15 @@ class GullAnimation  extends AnimationObject{
         for(int i = 0; i<18; i++) {
             ret.add("${baseLocationBody}${i}.png");
         }
-        //TODO make a real two tone palette
-        return new AnimationLayer(ret, new Palette());
+        return new AnimationLayer(ret, paletteSource, randomPalette);
     }
 
-    AnimationLayer hatLayers() {
-
+    AnimationLayer hatLayer() {
+        List<String> ret = new List<String>();
+        ret.add("${baseLocationHat}${hatBase}0.png");
+        for(int i = 0; i<17; i++) {
+            ret.add("${baseLocationHat}${hatBase}1.png");
+        }
+        return new AnimationLayer(ret, null,null);
     }
 }
--- web/scripts/Controllers/TombstoneTestController.dart
+++ web/scripts/Controllers/TombstoneTestController.dart
@@ -1,3 +1,4 @@
+import '../AnimationObject.dart';
 import '../CipherEngine.dart';
 import '../Locations/HuntingGrounds.dart';
 import '../NPCs/Tombstone.dart';
@@ -8,7 +9,14 @@ void main() {
     Element otherTest = new DivElement()..text = "it is not the Titan, nor the Reaper.";
     div.append(otherTest);
     CipherEngine.applyRandom(otherTest);
+    GullAnimation gull = new GullAnimation("pimp");
+    div.append(gull.element);
+
+    GullAnimation gull2 = new GullAnimation("pimp");
+    div.append(gull2.element);
+
     Tombstone t = new Tombstone();
     t.drawSelf(div);
+
 
 }
