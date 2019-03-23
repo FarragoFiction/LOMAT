43e7bf70094a9e314863878c3cdb54ac91be77a1
jadedResearcher <gtg287y@gmail.com>
2019-03-22 22:14:34 -0400

    the bot should click around blindly in dialogue options

--- web/scripts/GuideBot.dart
+++ web/scripts/GuideBot.dart
@@ -45,11 +45,28 @@ class GuideBot {
         DivElement button = querySelector("#talkButton");
         button.click();
         //TODO once the talk screen is up, click through all the text options i guess?
-        List<Element> dialogueOptions = querySelectorAll(".dialogueSelectableItem");
-        Random rand = new Random();
         //oh i see, react taught me about this
+        Random rand = new Random();
         new Timer(new Duration(milliseconds: 1000), () => {
-            rand.pickFrom(dialogueOptions).click()
+            dialogueLoop(rand,0)
+        });
+
+
+    }
+
+    void dialogueLoop(Random rand, int loop) {
+        List<Element> dialogueOptions = querySelectorAll(".dialogueSelectableItem");
+        if(dialogueOptions.isEmpty) {
+            //TODO maybe have some kind of bot status that flags its run out of things to do here
+            return;
+        }
+        if(loop <100) {
+            rand.pickFrom(dialogueOptions).click();
+        }else {
+            dialogueOptions.last.click(); //try to escape please
+        }
+        new Timer(new Duration(milliseconds: 1000), () => {
+            dialogueLoop(rand, loop+1)
         });
 
     }
