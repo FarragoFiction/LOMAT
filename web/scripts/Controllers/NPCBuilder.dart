54af7e8d4f10d95bd5420ae9d9d81066a3cb5506
jadedResearcher <gtg287y@gmail.com>
2019-06-19 21:13:00 -0400

    it begins

---
+++ web/scripts/Controllers/NPCBuilder.dart
@@ -0,0 +1,19 @@
+import 'package:CommonLib/Compression.dart';
+import 'package:CommonLib/Random.dart';
+
+import '../AnimationObject.dart';
+import '../CipherEngine.dart';
+import '../Locations/HuntingGrounds.dart';
+import '../Locations/Town.dart';
+import '../NPCs/Disease.dart';
+import '../NPCs/LOMATNPC.dart';
+import '../NPCs/NonGullLOMATNPC.dart';
+import '../NPCs/Tombstone.dart';
+import 'dart:html';
+
+import '../Sections/TalkySection.dart';
+
+DivElement div = querySelector('#output');
+void main()  async{
+    //TODO
+}
