a06b6b2c1123bb655eea83b28ad49f38ab076e4a
jadedResearcher <gtg287y@gmail.com>
2019-06-19 22:11:37 -0400

    builder nearly done, but loading crashes

--- web/scripts/Controllers/NPCBuilder.dart
+++ web/scripts/Controllers/NPCBuilder.dart
@@ -14,6 +14,64 @@ import 'dart:html';
 import '../Sections/TalkySection.dart';
 
 DivElement div = querySelector('#output');
+NPCBuilder builder;
 void main()  async{
-    //TODO
+    print("hello world");
+    NPCBuilder builder = new NPCBuilder();
+    builder.display(div);
+}
+
+//later extend this to make a gull builder, which also has palette etc.
+class NPCBuilder {
+    LOMATNPC npc;
+    InputElement nameElement = new InputElement();
+    TextAreaElement dataStringElement = new TextAreaElement()..cols = 100;
+    DivElement container = new DivElement()..id = "containerBuilder";
+
+    NPCBuilder() {
+        npc = NPCFactory.lilscumbag();
+        initDataElement();
+        initNameElement();
+        syncFormToNPC();
+    }
+
+    void syncFormToNPC() {
+        nameElement.value = npc.name;
+        dataStringElement.value = npc.toDataString();
+    }
+
+    void loadNPC() {
+        npc  = LOMATNPC.loadFromDataString(dataStringElement.value);
+    }
+
+    void syncNPCToForm() {
+        npc.name = nameElement.value;
+        print("I'm syncing the npc to the form, and its name should be ${npc.name}");
+        dataStringElement.value = npc.toDataString();
+    }
+
+    void initDataElement() {
+        DivElement div = new DivElement()..classes.add("formSection");
+        LabelElement dataLabel = new LabelElement()..text = "DataString";
+        div.append(dataLabel);
+        div.append(dataStringElement);
+        dataStringElement.onChange.listen((Event e) => loadNPC());
+        container.append(div);
+    }
+
+    void initNameElement() {
+        DivElement div = new DivElement()..classes.add("formSection");;
+        LabelElement dataLabel = new LabelElement()..text = "Name:";
+        div.append(dataLabel);
+        div.append(nameElement);
+        nameElement.onInput.listen((Event e) => syncNPCToForm());
+
+        container.append(div);
+    }
+
+
+    void display(Element parent) {
+        print("i'm trying to display, but what is happening?");
+        parent.append(container);
+    }
 }
