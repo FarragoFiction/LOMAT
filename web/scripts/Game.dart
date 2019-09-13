708d6c20612dd63c1ac75fa8731e5958e0807192
jadedResearcher <gtg287y@gmail.com>
2019-09-12 22:15:13 -0400

    wild"

--- web/scripts/Game.dart
+++ web/scripts/Game.dart
@@ -291,10 +291,9 @@ class Game
     }
 
     Future setStartingTown() async {
-        //Town town = new Town.dontevercallthisblindly("city2",findWanderingNPCS(),null,startingGenome());
-        Town town = Town.voidTown;
+        Town town = new Town.dontevercallthisblindly("city2",findWanderingNPCS(),null,startingGenome());
+        //Town town = voidTown;
         //don't overwrite genome
-       //await  town.initGenome();
         currentLocation = town;
     }
 
