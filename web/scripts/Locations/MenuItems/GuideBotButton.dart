284905e5a5738e796a457f515e79c98bff455755
jadedResearcher <gtg287y@gmail.com>
2020-01-11 18:30:05 -0500

    bot mode has better warning

--- web/scripts/Locations/MenuItems/GuideBotButton.dart
+++ web/scripts/Locations/MenuItems/GuideBotButton.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import '../../Game.dart';
 import '../../GuideBot.dart';
 import 'MenuHolder.dart';
 import 'MenuItem.dart';
@@ -9,7 +12,43 @@ class GuideBotButton extends MenuItem {
 
   @override
   void onClick() {
-    bot.toggle();
+    //TODO show popup instead
+    if(!bot.running) {
+      DivElement modal = new DivElement()..classes.add("flavorText");
+      String warning = "Are you SURE you want to turn off LOMAT's Bot mode? Are you prepared for the possibility that once you are no longer in control, you may not be able to get it back?";
+      modal.text = warning;
+      DivElement buttonHolder = new DivElement();
+      //TODO have two buttons, cancel, and abdicate
+      ButtonElement yes = new ButtonElement()
+        ..classes.add("voidButton")
+        ..text = "ABDICATE";
+      ButtonElement no = new ButtonElement()
+        ..classes.add("voidButton")
+        ..text = "ABJURE";
+      yes.onClick.listen((Event e) {
+        bot.toggle();
+        modal.text = "Don't say we didn't warn you, bro...";
+        dismissAfterTime(modal,1000);
+      });
+      buttonHolder.append(yes);
+      buttonHolder.append(no);
+      modal.append(buttonHolder);
+      Game.instance.currentLocation.container.append(modal);
+
+      no.onClick.listen((Event e) {
+        modal.remove();
+      });
+    }else {
+      bot.toggle();
+    }
+
+  }
+
+  void dismissAfterTime(Element element, int time) async {
+  new Timer(new Duration(milliseconds: time), () => {
+    element.remove()
+  });
+
   }
 
 
